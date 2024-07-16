0866f29bbbac0233ecf23fc8ed4db7b93d926ad2
souvikree <souvikg3225@gmail.com>
2024-07-16 16:59:50 +0530

    api endpoint update

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const cors = require("cors")
 
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT =  8080;
 const corsOptions ={
     origin:'*', 
     credentials:true,            
